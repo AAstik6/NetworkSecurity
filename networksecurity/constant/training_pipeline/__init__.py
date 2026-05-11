98763581bddb2e5d61c25b323da7d2dac2fe166b
AAstik6 <aastik829@gmail.com>
2026-05-12 03:07:37 +0530

    pushed data into cloud- artifact and pkl files

--- networksecurity/constant/training_pipeline/__init__.py
+++ networksecurity/constant/training_pipeline/__init__.py
@@ -68,3 +68,7 @@ MODEL_TRAINER_TRAINED_MODEL_DIR: str = "trained_model"
 MODEL_TRAINER_TRAINED_MODEL_NAME: str = "model.pkl"
 MODEL_TRAINER_EXPECTED_SCORE: float = 0.6
 MODEL_TRAINER_OVER_FIITING_UNDER_FITTING_THRESHOLD: float = 0.05
+
+TRAINING_BUCKET_NAME = "networksecurity-aastik2"
+
+## kill -9 $(lsof -t -i:8000)
